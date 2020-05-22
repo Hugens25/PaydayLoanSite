1fab3695729816168ef7149fbcd94a59ca3121d4
Hugens25 <hugens25@yahoo.com>
2020-05-22 01:58:56 -0500

    connected app to AWS

--- src/components/UserPages/Login.js
+++ src/components/UserPages/Login.js
@@ -8,6 +8,7 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import green from '@material-ui/core/colors/green';
+import red from '@material-ui/core/colors/red';
 
 const StyledButton = withStyles({
     root: {
@@ -44,11 +45,39 @@ const useStyles = makeStyles((theme) => ({
       marginTop: theme.spacing(3),
       marginLeft: theme.spacing(1),
     },
+    warning : {
+      color: red[500],
+    },
   }));
 
 export default function Login() {
 
-    const classes = useStyles();
+  const classes = useStyles();
+
+  const [email, setEmail] = React.useState();
+  const [password, setPassword] = React.useState();
+  const [validateCredentials, setValidateCredentials] = React.useState(true);
+
+  const handleEmaileEntry = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordEntry = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleValidateCredentials = (val) => {
+    setValidateCredentials(val);
+  };
+
+  async function handleLogin() {
+    let url = process.env.REACT_APP_VALIDATE_USER_URL
+    let payload = {'email':email, 'password_hash':password}
+    let data = await fetch(url, {method: 'POST', body: JSON.stringify(payload)})
+    console.log(await data.json())
+    handleValidateCredentials(data.status === 200 ? true : false)
+  }
+
     return(
       <React.Fragment>
         <CssBaseline />
@@ -58,15 +87,30 @@ export default function Login() {
               <Box mt={5}>
                   <Grid container spacing={4}>
                       <Grid item xs={12} sm={12}>
-                          <TextField required id="email" label="Username / Email" fullWidth autoComplete="email"/>
+                          <TextField 
+                            required id="email" 
+                            label="Username / Email" 
+                            fullWidth 
+                            autoComplete="email"
+                            onChange={handleEmaileEntry}
+                          />
                       </Grid>
                       <Grid item xs={12} sm={12}>
-                          <TextField required id="password" label="Password" type="password" fullWidth />
+                          <TextField 
+                            required id="password" 
+                            label="Password" 
+                            type="password" 
+                            fullWidth 
+                            onChange={handlePasswordEntry}
+                          />
                       </Grid>
                   </Grid>
               </Box>
+              <Box className={classes.warning}>
+                  {!validateCredentials && <Typography>Invalid Credentials Provided. Please try again.</Typography>}
+              </Box>
               <Box className={classes.buttons}>
-                  <StyledButton className={classes.button}>Login</StyledButton>
+                  <StyledButton className={classes.button} onClick={handleLogin}>Login</StyledButton>
               </Box>
           </Paper>
         </div>
