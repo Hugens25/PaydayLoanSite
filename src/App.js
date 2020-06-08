433a3dcd68ea7ac3910012249499b6085213f31b
Hugens25 <hugens25@yahoo.com>
2020-06-07 23:19:05 -0400

    added theme to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
 import './App.css';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
+const AppTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green[500],
+      contrastText: green[50],
+    },
+  },
+});
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <ThemeProvider theme={AppTheme}>
+        <Header />
+        <Footer />
+      </ThemeProvider>
+      
     </div>
   );
 }
