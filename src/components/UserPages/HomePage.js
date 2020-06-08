a33dcd234bbee8a21ec2dbc697cce9ed74a4ca62
Hugens Ulysse <hugens25@yahoo.com>
2020-06-07 20:56:14 -0400

    added spinner before Home Page upload

--- src/components/UserPages/HomePage.js
+++ src/components/UserPages/HomePage.js
@@ -6,6 +6,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
+import Spinner from '../misc/Spinner';
 
 const useStyles = makeStyles((theme) => ({
     layout: {
@@ -32,6 +33,13 @@ const useStyles = makeStyles((theme) => ({
       marginTop: theme.spacing(3),
       marginLeft: theme.spacing(1),
     },
+    spinnerContainer: {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
   }));
 
 export default function HomePage(props) {
@@ -50,7 +58,6 @@ export default function HomePage(props) {
   }
 
   const handleUserInfoIsFetched = () => {
-    // setUserInfo({firstName: "Hugens", lastName: "Ulysse"})
     setUserInfoIsFetched(true)
   }
 
@@ -66,14 +73,17 @@ export default function HomePage(props) {
           <div className={classes.layout}>
               <Box className={classes.container}>
                   <Paper className={classes.paper} elevation={3}>
-                      <Typography variant="h5" gutterBottom>My Account</Typography>
-                      {
-                        userInfoIsFetched &&
-                        <Typography >{`Welcome${userInfo.firstName ? ', '+userInfo.firstName : '' }${userInfo.lastName ? ' '+userInfo.lastName : ''}!`}</Typography>
+                      {!userInfoIsFetched ? (<Box className={classes.spinnerContainer}><Spinner /></Box>) : 
+                        (
+                          <Box>
+                            <Typography variant="h5" gutterBottom>My Account</Typography>
+                            <Typography >{`Welcome${userInfo.firstName ? ', '+userInfo.firstName : '' }${userInfo.lastName ? ' '+userInfo.lastName : ''}!`}</Typography>
+                            <Button><Link to='/settings'>Settings</Link></Button>
+                          </Box>
+                        )
                       }
-                      <Button><Link to='/settings'>Settings</Link></Button>
                   </Paper>
-                </Box>
+              </Box>
           </div>
       </Fragment>        
   );
